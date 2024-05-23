915d74ad310d86daba48266f1c082944311c4253
d4rk442 <114317537+d4rk442@users.noreply.github.com>
2024-05-24 01:06:19 +0800

    Create modemtune.sh

---
+++ modemtune.sh
@@ -0,0 +1,374 @@
+echo -e "START TUNING"
+
+rm -rf /etc/resolv.conf
+cat > /etc/resolv.conf <<-DNS
+nameserver 1.1.1.1
+nameserver 1.0.0.1
+DNS
+
+rm -rf /etc/opkg/distfeeds.conf
+cat > /etc/opkg/distfeeds.conf <<-DIST
+src/gz openwrt_base https://downloads.openwrt.org/snapshots/packages/aarch64_cortex-a53/base
+src/gz openwrt_luci https://downloads.openwrt.org/snapshots/packages/aarch64_cortex-a53/luci
+src/gz openwrt_packages https://downloads.openwrt.org/snapshots/packages/aarch64_cortex-a53/packages
+src/gz openwrt_routing https://downloads.openwrt.org/snapshots/packages/aarch64_cortex-a53/routing
+src/gz openwrt_telephony https://downloads.openwrt.org/snapshots/packages/aarch64_cortex-a53/telephony
+DIST
+
+opkg update
+opkg install irqbalance
+opkg install nano
+opkg install htop
+
+echo -e "REMOVE OPENVPN"
+opkg remove --autoremove luci-i18n-ipsec-server-zh-cn luci-app-ipsec-server strongswan-* 
+opkg remove --autoremove luci-i18n-ipsec-server-zh-cn luci-app-ipsec-server strongswan-* 
+opkg remove --autoremove luci-i18n-ipsec-server-zh-cn luci-app-ipsec-server strongswan-* 
+opkg remove --autoremove luci-i18n-openvpn-server-zh-cn luci-app-openvpn-server openvpn-*
+opkg remove --autoremove luci-i18n-openvpn-server-zh-cn luci-app-openvpn-server openvpn-*
+opkg remove --autoremove luci-i18n-openvpn-server-zh-cn luci-app-openvpn-server openvpn-*
+
+opkg update
+opkg install dnsmasq-full
+
+opkg remove --autoremove luci-i18n-zerotier-zh-cn luci-app-zerotier
+opkg remove --autoremove luci-i18n-zerotier-zh-cn luci-app-zerotier
+opkg remove --autoremove luci-i18n-zerotier-zh-cn luci-app-zerotier
+
+rm -rf /overlay/upper/etc/luci-app-ipsec-server
+rm -rf /overlay/upper/etc/mluci-i18n-ipsec-server-zh-cn
+rm -rf /overlay/upper/etc/luci-i18n-openvpn-server-zh-cn
+rm -rf /overlay/upper/etc/luci-openvpn
+
+rm -rf /overlay/upper/etc/config/luci-app-ipsec-server
+rm -rf /overlay/upper/etc/config/openvpn
+
+rm -rf /etc/config/luci-app-ipsec-server
+rm -rf /etc/config/luci-i18n-ipsec-server-zh-cn
+rm -rf /etc/config/luci-i18n-openvpn-server-zh-cn
+rm -rf /etc/config/luci-openvpn
+
+rm -rf /etc/config/luci-app-ipsec-server
+rm -rf /etc/config/openvpn
+
+opkg update
+opkg install dnsmasq-full
+
+echo -e "INSTALL DNSMASQ"
+rm -rf /etc/dnsmasq.conf
+cat > /etc/dnsmasq.conf <<-DNSMASQ
+#!/usr/bin/env bash
+log-facility=-
+DNSMASQ
+
+rm -rf /etc/resolv.conf
+cat > /etc/resolv.conf <<-DNS
+nameserver 1.1.1.1
+nameserver 1.0.0.1
+DNS
+
+rm -rf /tmp/resolv.conf
+cat > /tmp/resolv.conf <<-DNS
+search lan
+nameserver 127.0.0.1
+nameserver ::1
+DNS
+
+echo -e "BYPASS TTL64"
+cat > /overlay/upper/etc/ttl.user <<-TTLETC
+# TTL Setting
+#
+
+#startTTL1
+iptables -t mangle -I POSTROUTING -o wwan0 -j TTL --ttl-set 64
+iptables -t mangle -I POSTROUTING -o wwan0_1 -j TTL --ttl-set 64
+iptables -t mangle -I POSTROUTING -o br-lan -j TTL --ttl-set 64
+iptables -t mangle -A POSTROUTING -j TTL --ttl-set 64
+iptables -t mangle -I PREROUTING -i wwan0 -j TTL --ttl-set 64
+iptables -t mangle -I PREROUTING -i wwan0_1 -j TTL --ttl-set 64
+iptables -t mangle -I PREROUTING -i br-lan -j TTL --ttl-set 64
+iptables -t mangle -A PREROUTING -j TTL --ttl-set 64
+ip6tables -t mangle -A POSTROUTING -o wwan0 -j HL --hl-set 64
+ip6tables -t mangle -A POSTROUTING -o wwan0_1 -j HL --hl-set 64
+ip6tables -t mangle -A PREROUTING -i wwan0 -j HL --hl-set 64
+ip6tables -t mangle -A PREROUTING -i wwan0_1 -j HL --hl-set 64
+#endTTL
+TTLETC
+
+cat > /overlay/upper/etc/ttl.user.bk <<-TTLBK
+# TTL Setting
+#
+
+#startTTL1
+iptables -t mangle -I POSTROUTING -o wwan0 -j TTL --ttl-set 64
+iptables -t mangle -I POSTROUTING -o wwan0_1 -j TTL --ttl-set 64
+iptables -t mangle -I POSTROUTING -o br-lan -j TTL --ttl-set 64
+iptables -t mangle -A POSTROUTING -j TTL --ttl-set 64
+iptables -t mangle -I PREROUTING -i wwan0 -j TTL --ttl-set 64
+iptables -t mangle -I PREROUTING -i wwan0_1 -j TTL --ttl-set 64
+iptables -t mangle -I PREROUTING -i br-lan -j TTL --ttl-set 64
+iptables -t mangle -A PREROUTING -j TTL --ttl-set 64
+ip6tables -t mangle -A POSTROUTING -o wwan0 -j HL --hl-set 64
+ip6tables -t mangle -A POSTROUTING -o wwan0_1 -j HL --hl-set 64
+ip6tables -t mangle -A PREROUTING -i wwan0 -j HL --hl-set 64
+ip6tables -t mangle -A PREROUTING -i wwan0_1 -j HL --hl-set 64
+#endTTL
+TTLBK
+
+cat > /etc/ttl.user <<-TTLUSER
+# TTL Setting
+#
+
+#startTTL1
+iptables -t mangle -I POSTROUTING -o wwan0 -j TTL --ttl-set 64
+iptables -t mangle -I POSTROUTING -o wwan0_1 -j TTL --ttl-set 64
+iptables -t mangle -I POSTROUTING -o br-lan -j TTL --ttl-set 64
+iptables -t mangle -A POSTROUTING -j TTL --ttl-set 64
+iptables -t mangle -I PREROUTING -i wwan0 -j TTL --ttl-set 64
+iptables -t mangle -I PREROUTING -i wwan0_1 -j TTL --ttl-set 64
+iptables -t mangle -I PREROUTING -i br-lan -j TTL --ttl-set 64
+iptables -t mangle -A PREROUTING -j TTL --ttl-set 64
+ip6tables -t mangle -A POSTROUTING -o wwan0 -j HL --hl-set 64
+ip6tables -t mangle -A POSTROUTING -o wwan0_1 -j HL --hl-set 64
+ip6tables -t mangle -A PREROUTING -i wwan0 -j HL --hl-set 64
+ip6tables -t mangle -A PREROUTING -i wwan0_1 -j HL --hl-set 64
+#endTTL
+TTLUSER
+
+cat > /etc/ttl.user.bk <<-TTLUSERBK
+# TTL Setting
+#
+
+#startTTL1
+iptables -t mangle -I POSTROUTING -o wwan0 -j TTL --ttl-set 64
+iptables -t mangle -I POSTROUTING -o wwan0_1 -j TTL --ttl-set 64
+iptables -t mangle -I POSTROUTING -o br-lan -j TTL --ttl-set 64
+iptables -t mangle -A POSTROUTING -j TTL --ttl-set 64
+iptables -t mangle -I PREROUTING -i wwan0 -j TTL --ttl-set 64
+iptables -t mangle -I PREROUTING -i wwan0_1 -j TTL --ttl-set 64
+iptables -t mangle -I PREROUTING -i br-lan -j TTL --ttl-set 64
+iptables -t mangle -A PREROUTING -j TTL --ttl-set 64
+ip6tables -t mangle -A POSTROUTING -o wwan0 -j HL --hl-set 64
+ip6tables -t mangle -A POSTROUTING -o wwan0_1 -j HL --hl-set 64
+ip6tables -t mangle -A PREROUTING -i wwan0 -j HL --hl-set 64
+ip6tables -t mangle -A PREROUTING -i wwan0_1 -j HL --hl-set 64
+#endTTL
+TTLUSERBK
+
+cat > /overlay/upper/etc/firewall.user <<-FFE
+#!/bin/sh
+iptables -t mangle -I POSTROUTING -o wwan0 -j TTL --ttl-set 64
+iptables -t mangle -I POSTROUTING -o wwan0_1 -j TTL --ttl-set 64
+iptables -t mangle -I POSTROUTING -o br-lan -j TTL --ttl-set 64
+iptables -t mangle -A POSTROUTING -j TTL --ttl-set 64
+iptables -t mangle -I PREROUTING -i wwan0 -j TTL --ttl-set 64
+iptables -t mangle -I PREROUTING -i wwan0_1 -j TTL --ttl-set 64
+iptables -t mangle -I PREROUTING -i br-lan -j TTL --ttl-set 64
+iptables -t mangle -A PREROUTING -j TTL --ttl-set 64
+ip6tables -t mangle -A POSTROUTING -o wwan0 -j HL --hl-set 64
+ip6tables -t mangle -A POSTROUTING -o wwan0_1 -j HL --hl-set 64
+ip6tables -t mangle -A PREROUTING -i wwan0 -j HL --hl-set 64
+ip6tables -t mangle -A PREROUTING -i wwan0_1 -j HL --hl-set 64
+FFE
+
+cat > /etc/firewall.user <<-FFW
+#!/bin/sh
+iptables -t mangle -I POSTROUTING -o wwan0 -j TTL --ttl-set 64
+iptables -t mangle -I POSTROUTING -o wwan0_1 -j TTL --ttl-set 64
+iptables -t mangle -I POSTROUTING -o br-lan -j TTL --ttl-set 64
+iptables -t mangle -A POSTROUTING -j TTL --ttl-set 64
+iptables -t mangle -I PREROUTING -i wwan0 -j TTL --ttl-set 64
+iptables -t mangle -I PREROUTING -i wwan0_1 -j TTL --ttl-set 64
+iptables -t mangle -I PREROUTING -i br-lan -j TTL --ttl-set 64
+iptables -t mangle -A PREROUTING -j TTL --ttl-set 64
+ip6tables -t mangle -A POSTROUTING -o wwan0 -j HL --hl-set 64
+ip6tables -t mangle -A POSTROUTING -o wwan0_1 -j HL --hl-set 64
+ip6tables -t mangle -A PREROUTING -i wwan0 -j HL --hl-set 64
+ip6tables -t mangle -A PREROUTING -i wwan0_1 -j HL --hl-set 64
+FFW
+
+echo -e "TWEAK SYSCTL"
+rm -rf /etc/sysctl.conf
+cat > /etc/sysctl.conf <<-SYS1
+net.ipv4.ip_default_ttl=64
+net.ipv6.conf.all.hop_limit = 64
+fs.file-max = 1000000
+fs.inotify.max_user_instances = 8192
+net.ipv4.tcp_syncookies = 1
+net.ipv4.tcp_fin_timeout = 30
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.ip_local_port_range = 1024 65000
+net.ipv4.tcp_max_syn_backlog = 16384
+net.ipv4.tcp_max_tw_buckets = 6000
+net.ipv4.route.gc_timeout = 100
+net.ipv4.tcp_syn_retries = 1
+net.ipv4.tcp_synack_retries = 1
+net.ipv4.tcp_mtu_probing=1
+net.core.somaxconn = 32768
+net.core.netdev_max_backlog = 32768
+net.ipv4.tcp_timestamps = 0
+net.ipv4.tcp_max_orphans = 32768
+net.netfilter.nf_conntrack_max=65535
+net.core.rmem_max=67108864
+net.core.wmem_max=67108864
+net.core.rmem_default=256960
+net.core.wmem_default=256960
+net.ipv4.tcp_rmem=4096 87380 33554432
+net.ipv4.tcp_wmem=4096 65536 33554432
+net.ipv4.tcp_mem=131072  262144  524288
+net.ipv4.udp_rmem_min = 16384
+net.ipv4.udp_wmem_min = 16384
+net.ipv4.udp_mem = 65536 131072 262144
+net.netfilter.nf_conntrack_tcp_timeout_time_wait=30
+net.netfilter.nf_conntrack_tcp_timeout_fin_wait=30
+SYS1
+
+rm -rf /overlay/upper/etc/sysctl.d/*
+rm -rf /overlay/upper/etc/sysctl.d/qca-nss-ecm.conf
+cat > /overlay/upper/etc/sysctl.d/qca-nss-ecm.conf <<-SYS2
+#nf_conntrack_tcp_no_window_check is 0 by default, set it to 1
+net.netfilter.nf_conntrack_tcp_no_window_check=1
+net.netfilter.nf_conntrack_max=65535
+net.bridge.bridge-nf-call-arptables = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+SYS2
+
+rm -rf /overlay/upper/etc/sysctl.d/99-tweaker.conf
+cat > /overlay/upper/etc/sysctl.d/99-tweaker.conf <<-SYS
+net.ipv4.ip_default_ttl=64
+net.ipv6.conf.all.hop_limit = 64
+fs.file-max = 1000000
+fs.inotify.max_user_instances = 8192
+net.ipv4.tcp_syncookies = 1
+net.ipv4.tcp_fin_timeout = 30
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.ip_local_port_range = 1024 65000
+net.ipv4.tcp_max_syn_backlog = 16384
+net.ipv4.tcp_max_tw_buckets = 6000
+net.ipv4.route.gc_timeout = 100
+net.ipv4.tcp_syn_retries = 1
+net.ipv4.tcp_synack_retries = 1
+net.ipv4.tcp_mtu_probing=1
+net.core.somaxconn = 32768
+net.core.netdev_max_backlog = 32768
+net.ipv4.tcp_timestamps = 0
+net.ipv4.tcp_max_orphans = 32768
+net.netfilter.nf_conntrack_max=65535
+net.core.rmem_max=67108864
+net.core.wmem_max=67108864
+net.core.rmem_default=256960
+net.core.wmem_default=256960
+net.ipv4.tcp_rmem=4096 87380 33554432
+net.ipv4.tcp_wmem=4096 65536 33554432
+net.ipv4.tcp_mem=131072  262144  524288
+net.ipv4.udp_rmem_min = 16384
+net.ipv4.udp_wmem_min = 16384
+net.ipv4.udp_mem = 65536 131072 262144
+net.netfilter.nf_conntrack_tcp_timeout_time_wait=30
+net.netfilter.nf_conntrack_tcp_timeout_fin_wait=30
+SYS
+sysctl -p
+
+echo -e "TUNING NETWORK"
+rm -rf /overlay/upper/etc/config/network
+cat > /overlay/upper/etc/config/network <<-NETTY1
+config interface 'loopback'
+        option ifname 'lo'
+        option proto 'static'
+        option ipaddr '127.0.0.1'
+        option netmask '255.0.0.0'
+
+config globals 'globals'
+        option ula_prefix 'fd0d:1c3a:2ae7::/48'
+
+config interface 'lan'
+        option type 'bridge'
+        option ifname 'eth0 eth1 eth2 eth3 eth4'
+        option proto 'static'
+        option ipaddr '192.168.1.1'
+        option netmask '255.255.255.0'
+        option multicast_querier '0'
+        option igmp_snooping '0'
+        option ip6assign '60'
+        option force_link '1'
+
+config interface 'wan'
+        option ifname 'wwan0_1'
+        option proto 'dhcp'
+        option dns '1.1.1.1 1.0.0.1'
+        option metric '1'
+        option ttl '64'
+
+config interface 'wan6'
+        option ifname 'wwan0_1'
+        option proto 'dhcpv6'
+        option ttl '64'
+
+config interface 'wan1'
+        option proto 'dhcp'
+        option _orig_bridge 'false'
+        option metric '10'
+        option ttl '64'
+NETTY1
+
+cat > /etc/config/network <<-NETTY
+config interface 'loopback'
+        option ifname 'lo'
+        option proto 'static'
+        option ipaddr '127.0.0.1'
+        option netmask '255.0.0.0'
+
+config globals 'globals'
+        option ula_prefix 'fd0d:1c3a:2ae7::/48'
+
+config interface 'lan'
+        option type 'bridge'
+        option ifname 'eth0 eth1 eth2 eth3 eth4'
+        option proto 'static'
+        option ipaddr '192.168.1.1'
+        option netmask '255.255.255.0'
+        option multicast_querier '0'
+        option igmp_snooping '0'
+        option ip6assign '60'
+        option force_link '1'
+
+config interface 'wan'
+        option ifname 'wwan0_1'
+        option proto 'dhcp'
+        option dns '1.1.1.1 1.0.0.1'
+        option metric '1'
+        option ttl '64'
+
+config interface 'wan6'
+        option ifname 'wwan0_1'
+        option proto 'dhcpv6'
+        option ttl '64'
+
+config interface 'wan1'
+        option proto 'dhcp'
+        option _orig_bridge 'false'
+        option metric '10'
+        option ttl '64'
+NETTY
+
+echo -e "BYPASS SMP-TUNE"
+rm -rf /overlay/upper/etc/hotplug.d/net/20-smp-tune
+rm -rf /overlay/upper/etc/hotplug.d/net/99-smp-tune
+wget -O /overlay/upper/etc/hotplug.d/net/99-smp-tune https://raw.githubusercontent.com/d4rk442/tweak/main/99-smp-tune
+
+rm -rf /etc/hotplug.d/net/20-smp-tune
+rm -rf /etc/hotplug.d/net/99-smp-tune
+wget -O /etc/hotplug.d/net/99-smp-tune https://raw.githubusercontent.com/d4rk442/tweak/main/99-smp-tune
+
+echo -e "BYPASS IRQBALANCE"
+cat > /etc/config/irqbalance <<-IRQ
+config irqbalance 'irqbalance'
+             option enable '1'
+
+             option interval '1'
+IRQ
+echo -e "FINISH SCRIPT REBOOT............"
+
+rm -rf /root/*
+reboot
