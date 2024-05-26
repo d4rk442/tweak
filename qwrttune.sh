dc3192b9e6bea87f933a9578d696aceef1cb799d
d4rk442 <114317537+d4rk442@users.noreply.github.com>
2024-05-27 03:20:58 +0800

    Create qwrttune.sh

---
+++ qwrttune.sh
@@ -0,0 +1,212 @@
+echo -e "START TUNING"
+rm -rf /etc/resolv.conf
+cat > /etc/resolv.conf <<-DNS
+nameserver 1.1.1.1
+nameserver 1.0.0.1
+DNS
+
+rm -rf /etc/opkg/distfeeds.conf
+cat > /etc/opkg/distfeeds.conf <<-DIST
+src/gz openwrt_base https://downloads.immortalwrt.org/releases/21.02.1/packages/aarch64_cortex-a53/base
+src/gz openwrt_luci https://downloads.immortalwrt.org/releases/21.02.1/packages/aarch64_cortex-a53/luci
+src/gz openwrt_packages https://downloads.immortalwrt.org/releases/21.02.1/packages/aarch64_cortex-a53/packages
+src/gz openwrt_routing https://downloads.immortalwrt.org/releases/21.02.1/packages/aarch64_cortex-a53/routing
+src/gz openwrt_telephony https://downloads.immortalwrt.org/releases/21.02.1/packages/aarch64_cortex-a53/telephony
+DIST
+
+opkg update
+opkg install irqbalance
+opkg install nano-full
+opkg install htop
+opkg install sudo
+
+echo -e "REMOVE OPENVPN"
+opkg remove --autoremove luci-i18n-ipsec-server-zh-cn luci-app-ipsec-server strongswan-* 
+opkg remove --autoremove luci-i18n-ipsec-server-zh-cn luci-app-ipsec-server strongswan-* 
+opkg remove --autoremove luci-i18n-ipsec-server-zh-cn luci-app-ipsec-server strongswan-* 
+opkg remove --autoremove luci-i18n-openvpn-server-zh-cn luci-app-openvpn-server openvpn-*
+opkg remove --autoremove luci-i18n-openvpn-server-zh-cn luci-app-openvpn-server openvpn-*
+opkg remove --autoremove luci-i18n-openvpn-server-zh-cn luci-app-openvpn-server openvpn-*
+
+opkg update
+opkg install dnsmasq-full
+
+opkg remove --autoremove luci-i18n-zerotier-zh-cn luci-app-zerotier
+opkg remove --autoremove luci-i18n-zerotier-zh-cn luci-app-zerotier
+opkg remove --autoremove luci-i18n-zerotier-zh-cn luci-app-zerotier
+
+rm -rf /overlay/upper/etc/luci-app-ipsec-server
+rm -rf /overlay/upper/etc/luci-i18n-ipsec-server-zh-cn
+rm -rf /overlay/upper/etc/luci-i18n-openvpn-server-zh-cn
+rm -rf /overlay/upper/etc/luci-openvpn
+
+rm -rf /overlay/upper/etc/config/luci-app-ipsec-server
+rm -rf /overlay/upper/etc/config/openvpn
+
+rm -rf /etc/config/luci-app-ipsec-server
+rm -rf /etc/config/luci-i18n-ipsec-server-zh-cn
+rm -rf /etc/config/luci-i18n-openvpn-server-zh-cn
+rm -rf /etc/config/luci-openvpn
+
+rm -rf /etc/config/luci-app-ipsec-server
+rm -rf /etc/config/openvpn
+
+opkg update
+opkg install dnsmasq-full
+
+echo -e "INSTALL DNSMASQ"
+rm -rf /etc/dnsmasq.conf
+cat > /etc/dnsmasq.conf <<-DNSMASQ
+#!/usr/bin/env bash
+log-facility=-
+DNSMASQ
+
+rm -rf /etc/resolv.conf
+cat > /etc/resolv.conf <<-DNS
+search lan
+nameserver 127.0.0.1
+nameserver ::1
+DNS
+
+echo -e "BYPASS TTL64"
+
+uci set cpufreq.cpufreq.governor=ondemand;
+uci set cpufreq.cpufreq.minifreq=2208000;
+uci commit cpufreq;
+uci set turboacc.config.bbr_cca=0;
+uci commit turboacc;
+uci set system.@system[0].zonename='Asia/Kuala Lumpur';
+uci commit system;
+uci set luci.main.lang='auto';
+uci commit luci.main;
+uci -q delete system.ntp.server;
+uci add_list system.ntp.server='my.pool.ntp.org';
+uci add_list system.ntp.server='ntp.google.com';
+uci add_list system.ntp.server='ntp.windows.com';
+uci add_list system.ntp.server='ntp.cloudflare.com';
+uci commit system.ntp;
+/etc/init.d/sysntpd restart;
+uci set network.wan.ifname='wwan0_1';
+uci commit network.wan;
+uci set network.wan6.ifname='wwan0_1';
+uci commit network.wan6
+
+rm -rf /overlay/upper/etc/firewall.user
+cat > /overlay/upper/etc/firewall.user <<-FFE
+#!/bin/sh
+iptables -t mangle -I POSTROUTING -o br-lan -j TTL --ttl-set 64
+iptables -t mangle -I POSTROUTING -o wwan0 -j TTL --ttl-set 64
+iptables -t mangle -I POSTROUTING -o wwan0_1 -j TTL --ttl-set 64
+iptables -t mangle -I PREROUTING -i br-lan -j TTL --ttl-set 64
+iptables -t mangle -I PREROUTING -i wwan0 -j TTL --ttl-set 64
+iptables -t mangle -I PREROUTING -i wwan0_1 -j TTL --ttl-set 64
+ip6tables -t mangle -I POSTROUTING -o wwan0 -j HL --hl-set 64
+ip6tables -t mangle -I POSTROUTING -o wwan0_1 -j HL --hl-set 64
+ip6tables -t mangle -I PREROUTING -i wwan0 -j HL --hl-set 64
+ip6tables -t mangle -I PREROUTING -i wwan0_1 -j HL --hl-set 64
+FFE
+
+rm -rf /etc/firewall.user
+cat > /etc/firewall.user <<-FFW
+#!/bin/sh
+iptables -t mangle -I POSTROUTING -o br-lan -j TTL --ttl-set 64
+iptables -t mangle -I POSTROUTING -o wwan0 -j TTL --ttl-set 64
+iptables -t mangle -I POSTROUTING -o wwan0_1 -j TTL --ttl-set 64
+iptables -t mangle -I PREROUTING -i br-lan -j TTL --ttl-set 64
+iptables -t mangle -I PREROUTING -i wwan0 -j TTL --ttl-set 64
+iptables -t mangle -I PREROUTING -i wwan0_1 -j TTL --ttl-set 64
+ip6tables -t mangle -I POSTROUTING -o wwan0 -j HL --hl-set 64
+ip6tables -t mangle -I POSTROUTING -o wwan0_1 -j HL --hl-set 64
+ip6tables -t mangle -I PREROUTING -i wwan0 -j HL --hl-set 64
+ip6tables -t mangle -I PREROUTING -i wwan0_1 -j HL --hl-set 64
+FFW
+
+rm -rf /overlay/upper/etc/ttl.user.bk
+cat > /overlay/upper/etc/ttl.user.bk <<-FFE
+#!/bin/sh
+iptables -t mangle -I POSTROUTING -o br-lan -j TTL --ttl-set 64
+iptables -t mangle -I POSTROUTING -o wwan0 -j TTL --ttl-set 64
+iptables -t mangle -I POSTROUTING -o wwan0_1 -j TTL --ttl-set 64
+iptables -t mangle -I PREROUTING -i br-lan -j TTL --ttl-set 64
+iptables -t mangle -I PREROUTING -i wwan0 -j TTL --ttl-set 64
+iptables -t mangle -I PREROUTING -i wwan0_1 -j TTL --ttl-set 64
+ip6tables -t mangle -I POSTROUTING -o wwan0 -j HL --hl-set 64
+ip6tables -t mangle -I POSTROUTING -o wwan0_1 -j HL --hl-set 64
+ip6tables -t mangle -I PREROUTING -i wwan0 -j HL --hl-set 64
+ip6tables -t mangle -I PREROUTING -i wwan0_1 -j HL --hl-set 64
+FFE
+
+rm -rf /etc/ttl.user.bk
+cat > /etc/ttl.user.bk <<-FFW
+#!/bin/sh
+iptables -t mangle -I POSTROUTING -o br-lan -j TTL --ttl-set 64
+iptables -t mangle -I POSTROUTING -o wwan0 -j TTL --ttl-set 64
+iptables -t mangle -I POSTROUTING -o wwan0_1 -j TTL --ttl-set 64
+iptables -t mangle -I PREROUTING -i br-lan -j TTL --ttl-set 64
+iptables -t mangle -I PREROUTING -i wwan0 -j TTL --ttl-set 64
+iptables -t mangle -I PREROUTING -i wwan0_1 -j TTL --ttl-set 64
+ip6tables -t mangle -I POSTROUTING -o wwan0 -j HL --hl-set 64
+ip6tables -t mangle -I POSTROUTING -o wwan0_1 -j HL --hl-set 64
+ip6tables -t mangle -I PREROUTING -i wwan0 -j HL --hl-set 64
+ip6tables -t mangle -I PREROUTING -i wwan0_1 -j HL --hl-set 64
+FFW
+
+echo -e "BYPASS SMP-TUNE"
+chmod +x /overlay/upper/etc/hotplug.d/net/20-smp-tune
+rm -rf /overlay/upper/etc/hotplug.d/net/20-smp-tune
+rm -rf /overlay/upper/etc/hotplug.d/net/99-smp-tune
+wget -O /overlay/upper/etc/hotplug.d/net/99-smp-tune https://raw.githubusercontent.com/d4rk442/tweak/main/99-smp-tune
+chmod 755 /overlay/upper/etc/hotplug.d/net/99-smp-tune
+
+chmod +x /etc/hotplug.d/net/20-smp-tune
+rm -rf /etc/hotplug.d/net/20-smp-tune
+rm -rf /etc/hotplug.d/net/99-smp-tune
+wget -O /etc/hotplug.d/net/99-smp-tune https://raw.githubusercontent.com/d4rk442/tweak/main/99-smp-tune
+chmod 755 /etc/hotplug.d/net/99-smp-tune
+
+echo -e "BYPASS IRQBALANCE"
+rm -rf /etc/config/irqbalance
+cat > /etc/config/irqbalance <<-IRQ
+config irqbalance 'irqbalance'
+             option enable '1'
+
+             option interval '1'
+IRQ
+chmod 755 /etc/config/irqbalance
+
+echo -e "TRANSLATE ALL ENGLISH"
+wget -q -O /usr/lib/lua/luci/model/cbi/rooter/customize.lua "https://github.com/NevermoreSSH/openwrt-packages2/releases/download/arca_presetv2/customize.lua";
+wget -q -O /usr/lib/lua/luci/view/rooter/debug.htm "https://github.com/NevermoreSSH/openwrt-packages2/releases/download/arca_presetv2/debug.htm";
+wget -q -O /usr/lib/lua/luci/view/rooter/misc.htm "https://github.com/NevermoreSSH/openwrt-packages2/releases/download/arca_presetv2/misc.htm";
+wget -q -O /usr/lib/lua/luci/controller/admin/modem.lua "https://github.com/NevermoreSSH/openwrt-packages2/releases/download/arca_presetv2/modem.lua";
+wget -q -O /usr/lib/lua/luci/view/modlog/modlog.htm "https://github.com/NevermoreSSH/openwrt-packages2/releases/download/arca_presetv2/modlog.htm";
+wget -q -O /usr/lib/lua/luci/controller/modlog.lua "https://github.com/NevermoreSSH/openwrt-packages2/releases/download/arca_presetv2/modlog.lua";
+wget -q -O /usr/lib/lua/luci/view/rooter/net_status.htm "https://github.com/NevermoreSSH/openwrt-packages2/releases/download/arca_presetv2/net_status.htm";
+wget -q -O /usr/lib/lua/luci/model/cbi/rooter/profiles.lua "https://github.com/NevermoreSSH/openwrt-packages2/releases/download/arca_presetv2/profiles.lua";
+wget -q -O /usr/lib/lua/luci/view/rooter/sms.htm "https://github.com/NevermoreSSH/openwrt-packages2/releases/download/arca_presetv2/sms.htm";
+wget -q -O /usr/lib/lua/luci/controller/sms.lua "https://github.com/NevermoreSSH/openwrt-packages2/releases/download/arca_presetv2/sms.lua";
+wget -q -O /usr/lib/lua/luci/view/rooter/custom.htm "https://github.com/NevermoreSSH/openwrt-packages2/releases/download/arca_presetv2/custom.htm";
+
+echo -e "TUNING NETWORK"
+rm -rf /etc/rc.local
+cat > /etc/rc.local <<-RCD
+#!/bin/sh -e
+# rc.local
+# By default this script does nothing.
+/etc/init.d/irqbalance start
+echo ondemand > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo ondemand > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
+echo ondemand > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
+echo ondemand > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
+sysctl net.core.default_qdisc=cake
+sysctl net.ipv4.tcp_congestion_control=bbr
+exit 0
+RCD
+chmod +x /etc/rc.local
+/etc/rc.local enable
+/etc/rc.local start
+
+echo -e "FINISH SCRIPT REBOOT............"
+
+rm -rf /root/*
+reboot
