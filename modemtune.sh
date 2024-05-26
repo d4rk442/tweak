1763e0a1d16decbabe4767262794be16edb91224
d4rk442 <114317537+d4rk442@users.noreply.github.com>
2024-05-27 03:18:30 +0800

    Update modemtune.sh

--- modemtune.sh
+++ modemtune.sh
@@ -7,11 +7,11 @@ DNS
 
 rm -rf /etc/opkg/distfeeds.conf
 cat > /etc/opkg/distfeeds.conf <<-DIST
-src/gz openwrt_base https://downloads.openwrt.org/snapshots/packages/aarch64_cortex-a53/base
-src/gz openwrt_luci https://downloads.openwrt.org/snapshots/packages/aarch64_cortex-a53/luci
-src/gz openwrt_packages https://downloads.openwrt.org/snapshots/packages/aarch64_cortex-a53/packages
-src/gz openwrt_routing https://downloads.openwrt.org/snapshots/packages/aarch64_cortex-a53/routing
-src/gz openwrt_telephony https://downloads.openwrt.org/snapshots/packages/aarch64_cortex-a53/telephony
+src/gz openwrt_base https://downloads.immortalwrt.org/releases/21.02.1/packages/aarch64_cortex-a53/base
+src/gz openwrt_luci https://downloads.immortalwrt.org/releases/21.02.1/packages/aarch64_cortex-a53/luci
+src/gz openwrt_packages https://downloads.immortalwrt.org/releases/21.02.1/packages/aarch64_cortex-a53/packages
+src/gz openwrt_routing https://downloads.immortalwrt.org/releases/21.02.1/packages/aarch64_cortex-a53/routing
+src/gz openwrt_telephony https://downloads.immortalwrt.org/releases/21.02.1/packages/aarch64_cortex-a53/telephony
 DIST
 
 opkg update
