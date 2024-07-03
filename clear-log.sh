77bc6b2a8dec8af5535637dc77739997e35c8adf
d4rk442 <114317537+d4rk442@users.noreply.github.com>
2024-07-04 01:37:58 +0800

    Create clear-log.sh

---
+++ clear-log.sh
@@ -0,0 +1,25 @@
+#wget https://github.com/${GitUser}/
+data=(`find /var/log/ -name *.log`);
+for log in "${data[@]}"
+do
+echo "$log clear"
+echo > $log
+done
+data=(`find /var/log/ -name *.err`);
+for log in "${data[@]}"
+do
+echo "$log clear"
+echo > $log
+done
+data=(`find /var/log/ -name mail.*`);
+for log in "${data[@]}"
+do
+echo "$log clear"
+echo > $log
+done
+echo > /var/log/syslog
+echo > /var/log/btmp
+echo > /var/log/messages
+echo > /var/log/debug
+echo -e "Script By Xtech"
+clear
