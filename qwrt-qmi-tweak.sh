36760937f1dec9be778d91be4b7fdbe9db2e62d7
d4rk442 <114317537+d4rk442@users.noreply.github.com>
2024-08-04 03:33:10 +0800

    Update qwrt-qmi-tweak.sh

--- qwrt-qmi-tweak.sh
+++ qwrt-qmi-tweak.sh
@@ -54,9 +54,15 @@ uci commit network.lan;
 uci set network.wan.peerdns='0';
 uci delete network.wan.dns;
 uci commit network.wan;
+uci set network.wan6.peerdns='0';
+uci delete network.wan6.dns;
+uci commit network.wan6;
+uci set network.wan1.peerdns='0';
+uci delete network.wan1.dns;
+uci commit network.wan1;
 uci set firewall.@defaults[0].flow_offloading='0';
 uci set firewall.@defaults[0].flow_offloading_hw='0';
-uci commit
+uci commit firewall
 
 echo -e "BYPASS-DNSMASQ"
 rm -rf /etc/dnsmasq.conf
@@ -135,9 +141,11 @@ kernel.panic=3
 
 kernel.core_pattern=/tmp/%e.%t.%p.%s.core
 
+net.ipv4.ip_forward=1
+net.ipv6.conf.all.forwarding=1
+net.ipv6.conf.default.forwarding=1
 net.ipv4.conf.default.arp_ignore=1
 net.ipv4.conf.all.arp_ignore=1
-net.ipv4.ip_forward=1
 net.ipv4.icmp_echo_ignore_broadcasts=1
 net.ipv4.icmp_ignore_bogus_error_responses=1
 net.ipv4.icmp_echo_ignore_all=1
@@ -147,13 +155,7 @@ net.ipv4.tcp_keepalive_time=120
 net.ipv4.tcp_keepalive_intvl=30
 net.ipv4.tcp_keepalive_probes=5
 net.ipv4.tcp_slow_start_after_idle=0
-net.ipv4.tcp_syncookies=1
-net.ipv4.tcp_timestamps=1
-net.ipv4.tcp_sack=1
-net.ipv4.tcp_dsack=1
 net.ipv4.tcp_mtu_probing=1
-net.ipv6.conf.default.forwarding=1
-net.ipv6.conf.all.forwarding=1
 net.netfilter.nf_conntrack_acct=1
 net.netfilter.nf_conntrack_checksum=0
 net.netfilter.nf_conntrack_max=16384
@@ -169,20 +171,21 @@ CUSTOM
 cat > /etc/sysctl.d/custom-bbr.conf <<-BBR
 net.core.default_qdisc=fq_codel
 net.ipv4.tcp_congestion_control=bbr
-net.core.somaxconn=4096
-net.core.rmem_default=1048576
-net.core.rmem_max=16777216
-net.core.netdev_max_backlog=16384
-net.core.wmem_default=1048576
-net.core.wmem_max=16777216
-net.core.optmem_max=65536
-net.ipv4.tcp_rmem=4096 1048576 2097152
-net.ipv4.tcp_wmem=4096 65536 16777216
-net.ipv4.udp_rmem_min=4096
-net.ipv4.udp_wmem_min=4096
-net.ipv4.tcp_max_syn_backlog=4096
-net.ipv4.tcp_max_tw_buckets=2000000
-net.ipv4.ip_local_port_range=10000 65535
+net.ipv4.tcp_ecn=0
+net.ipv4.route.flush=1
+net.ipv4.tcp_rfc1337=1
+net.ipv4.ip_no_pmtu_disc=0
+net.ipv4.tcp_sack=1
+net.ipv4.tcp_fack=1
+net.ipv4.tcp_dsack=1
+net.ipv4.tcp_window_scaling=1
+net.ipv4.tcp_timestamps=1
+net.ipv4.tcp_syncookies=1
+net.ipv4.tcp_rmem=4096 39000 187000
+net.ipv4.tcp_wmem=4096 39000 187000
+net.ipv4.tcp_mem=187000 187000 187000
+net.ipv4.tcp_no_metrics_save=1
+net.ipv4.tcp_moderate_rcvbuf=1
 BBR
 
 echo -e "TWEAKER-BIASA"
