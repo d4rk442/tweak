5bfba2ca8710d4f15a46ca615e789aa8d3e42d13
d4rk442 <114317537+d4rk442@users.noreply.github.com>
2024-10-18 20:15:02 +0800

    Update qwrt-qmi-tweak.sh

--- qwrt-qmi-tweak.sh
+++ qwrt-qmi-tweak.sh
@@ -1,8 +1,8 @@
 echo -e "SCRIPT-START"
 rm -rf /etc/resolv.conf
 cat > /etc/resolv.conf <<-DNS
-nameserver 1.1.1.2
-nameserver 1.0.0.2
+nameserver 1.1.1.1
+nameserver 1.0.0.1
 DNS
 
 echo -e "CHANGE-FEEDS"
@@ -38,24 +38,37 @@ uci set system.@system[0].zonename='Asia/Kuala Lumpur';
 uci commit system;
 uci set luci.main.lang='auto';
 uci commit luci.main;
+uci -q delete system.ntp.server;
+uci add_list system.ntp.server='my.pool.ntp.org';
+uci add_list system.ntp.server='ntp.google.com';
+uci add_list system.ntp.server='ntp.windows.com';
+uci add_list system.ntp.server='ntp.cloudflare.com';
 uci set network.wan.ifname='wwan0_1';
 uci commit network.wan;
 uci set network.wan6.ifname='wwan0_1';
 uci commit network.wan6;
 uci set network.lan.dns='1.1.1.1 2606:4700:4700::1111';
 uci commit network.lan;
+uci set firewall.@defaults[0].flow_offloading='0';
+uci set firewall.@defaults[0].flow_offloading_hw='0';
 uci commit firewall;
 uci set network.globals.packet_steering=1;
-uci commit network;
-uci set network.wan1.peerdns='0';
-uci delete network.wan1.dns;
-uci commit network.wan1;
 uci set network.wan.peerdns='0';
 uci delete network.wan.dns;
 uci commit network.wan;
+uci set network.wan1.peerdns='0';
+uci delete network.wan1.dns;
+uci commit network.wan1;
 uci set network.wan6.peerdns='0';
 uci delete network.wan6.dns;
 uci commit network.wan6;
+uci commit network;
+uci commit;
+
+uci set dhcp.wan6=dhcp;
+uci set dhcp.wan6.interface='wan6';
+uci set dhcp.wan6.ignore='1';
+uci commit dhcp;
 
 echo -e "BYPASS-DNSMASQ"
 rm -rf /etc/config/dhcp-opkg
@@ -65,50 +78,74 @@ cat > /etc/dnsmasq.conf <<-DNSMASQ
 #!/usr/bin/env bash
 bind-dynamic
 bogus-priv
+no-resolv
+strict-order
 log-facility=-
 local-ttl=60
 interface=*
+server=1.1.1.1
+server=1.0.0.1
 DNSMASQ
 
 echo -e "BYPASS-TTL"
-cat > /etc/firewall.d/firewall.user <<-TTL
-iptables -t mangle -F
-ip6tables -t mangle -F
-ip6tables -t mangle -I POSTROUTING -o wwan0 -j HL --hl-set 64
-ip6tables -t mangle -I PREROUTING -i wwan0 -j HL --hl-set 64
-iptables -t mangle -I POSTROUTING -o wwan0 -j TTL --ttl-set 64
-iptables -t mangle -I PREROUTING -i wwan0 -j TTL --ttl-set 64
-ip6tables -t mangle -I POSTROUTING -o wwan0_1 -j HL --hl-set 64
-ip6tables -t mangle -I PREROUTING -i wwan0_1 -j HL --hl-set 64
-iptables -t mangle -I POSTROUTING -o wwan0_1 -j TTL --ttl-set 64
-iptables -t mangle -I PREROUTING -i wwan0_1 -j TTL --ttl-set 64
-TTL
-chmod 755 /etc/firewall.d/firewall.user
-uci commit firewall
+rm -rf /etc/init.d/firewall-custom
+cat > /etc/init.d/firewall-custom <<-FRW
+#!/bin/sh /etc/rc.common
 
-cat /dev/ttyUSB2 &
-echo "AT+CMGR=1" > /dev/ttyUSB2
-echo "AT+CFUN=1" > /dev/ttyUSB2
+START=99
+
+start() {
+
+logger -t firewall-custom "Starting custom firewall rules"
+
+iptables -t mangle -I POSTROUTING -o wwan0 -j TTL --ttl-set 64
+iptables -t mangle -I POSTROUTING -o wwan0_1 -j TTL --ttl-set 64
+iptables -t mangle -I PREROUTING -i wwan0 -j TTL --ttl-set 64
+iptables -t mangle -I PREROUTING -i wwan0_1 -j TTL --ttl-set 64
+ip6tables -t mangle -I POSTROUTING -o wwan0 -j HL --hl-set 64
+ip6tables -t mangle -I POSTROUTING -o wwan0_1 -j HL --hl-set 64
+ip6tables -t mangle -I PREROUTING -i wwan0 -j HL --hl-set 64
+ip6tables -t mangle -I PREROUTING -i wwan0_1 -j HL --hl-set 64
+
+}
+
+stop() {
+
+logger -t firewall-custom "Stopping custom firewall rules"
+
+iptables -t mangle -D POSTROUTING -o wwan0 -j TTL --ttl-set 64
+iptables -t mangle -D POSTROUTING -o wwan0_1 -j TTL --ttl-set 64
+iptables -t mangle -D PREROUTING -i wwan0 -j TTL --ttl-set 64
+iptables -t mangle -D PREROUTING -i wwan0_1 -j TTL --ttl-set 64
+ip6tables -t mangle -D POSTROUTING -o wwan0 -j HL --hl-set 64
+ip6tables -t mangle -D POSTROUTING -o wwan0_1 -j HL --hl-set 64
+ip6tables -t mangle -D PREROUTING -i wwan0 -j HL --hl-set 64
+ip6tables -t mangle -D PREROUTING -i wwan0_1 -j HL --hl-set 64
+
+}
+FRW
+chmod +x /etc/init.d/firewall-custom
 
 echo -e "TWEAK-SPEED-SYSCTL"
-rm -rf /etc/sysctl.d/10-default.conf
+rm -rf /etc/sysctl.d/*
 cat > /etc/sysctl.d/10-default.conf <<-DEF
 kernel.panic=3
 kernel.core_pattern=/tmp/%e.%t.%p.%s.core
-fs.suid_dumpable=2
-
-fs.protected_hardlinks=1
-fs.protected_symlinks=1
 
 net.ipv4.conf.default.arp_ignore=1
 net.ipv4.conf.all.arp_ignore=1
 net.ipv4.icmp_echo_ignore_broadcasts=1
 net.ipv4.icmp_ignore_bogus_error_responses=1
+net.ipv4.icmp_echo_ignore_all=1
+net.ipv4.icmp_errors_use_inbound_ifaddr=0
 net.ipv4.igmp_max_memberships=100
-net.ipv4.tcp_fin_timeout=30
-net.ipv4.tcp_keepalive_time=120
+net.ipv4.tcp_fin_timeout=15
+net.ipv4.tcp_keepalive_intvl=30
+net.ipv4.tcp_keepalive_probes=5
+net.ipv4.tcp_tw_reuse=1
 net.ipv4.tcp_syncookies=1
-net.ipv4.tcp_timestamps=1                                                                 net.ipv4.tcp_sack=1
+net.ipv4.tcp_timestamps=1
+net.ipv4.tcp_sack=1
 net.ipv4.tcp_dsack=1
 
 net.ipv4.ip_forward=1
@@ -118,7 +155,6 @@ net.ipv6.conf.all.disable_ipv6=0
 net.ipv6.conf.default.disable_ipv6=0
 DEF
 
-rm -rf /etc/sysctl.d/11-nf-conntrack.conf
 cat > /etc/sysctl.d/11-nf-conntrack.conf <<-CONS
 net.netfilter.nf_conntrack_acct=1
 net.netfilter.nf_conntrack_checksum=0
@@ -131,7 +167,6 @@ net.netfilter.nf_conntrack_udp_timeout=60
 net.netfilter.nf_conntrack_udp_timeout_stream=180
 CONS
 
-rm -rf /etc/sysctl.d/custom-bbr.conf
 cat > /etc/sysctl.d/custom-bbr.conf <<-BBR
 net.core.rmem_default=65536
 net.core.wmem_default=65536
@@ -142,6 +177,7 @@ net.ipv4.tcp_wmem=4096 65536 16777216
 net.ipv4.tcp_rmem=4096 65536 16777216
 net.ipv4.tcp_mem=4096 65536 16777216
 net.ipv4.udp_mem=4096 65536 16777216
+net.ipv4.ping_group_range=0 429296729
 net.ipv4.udp_rmem_min=4096
 net.ipv4.udp_wmem_min=4096
 net.core.netdev_max_backlog=3000
@@ -165,6 +201,7 @@ config irqbalance 'irqbalance'
 
              option interval '1'
 IRQ
+chmod +x /etc/config/irqbalance
 
 rm -rf /etc/resolv.conf
 cat > /etc/resolv.conf <<-DNS
@@ -188,62 +225,25 @@ rm -rf /etc/rc.local
 cat > /etc/rc.local <<-RCD
 #TWEAK
 sysctl net.ipv4.tcp_congestion_control=bbr
-echo f > /sys/class/net/br-lan/queues/rx-0/rps_cpus
-echo f > /sys/class/net/wwan0/queues/rx-0/rps_cpus
-echo f > /sys/class/net/wwan0_1/queues/rx-0/rps_cpus
 echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 echo performance > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
 echo performance > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
 echo performance > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
 exit 0
 RCD
-
-rm -rf /etc/config/wireless
-cat > /etc/config/wireless <<-WIFI
-
-config wifi-device 'wifi0'
-        option type 'qcawificfg80211'
-        option macaddr 'ec:6c:9a:b8:4c:e0'
-        option hwmode '11axa'
-        option country 'US'
-        option channel '128'
-        option htmode 'HT160'
-        option txpower '30'
-
-config wifi-iface 'ath0'
-        option device 'wifi0'
-        option network 'lan'
-        option mode 'ap'
-        option wmm '1'
-        option rrm '1'
-        option qbssload '1'
-        option ssid 'WK-VISTANA-5G'
-        option encryption 'psk'
-        option key '112233445566'
-
-config wifi-device 'wifi1'
-        option type 'qcawificfg80211'
-        option channel '1'
-        option macaddr 'ec:6c:9a:b8:4c:df'
-        option hwmode '11axg'
-        option country 'US'
-        option htmode 'HT20'
-        option txpower '30'
-
-config wifi-iface 'ath1'
-        option device 'wifi1'
-        option network 'lan'
-        option mode 'ap'
-        option wmm '1'
-        option rrm '1'
-        option qbssload '1'
-        option ssid 'WK-VISTANA-2.4'
-        option encryption 'psk'
-        option key '112233445566'
-WIFI
-
 chmod +x /etc/rc.local
+
+uci commit
+uci commit firewall
+uci commit network
+/etc/init.d/firewall-custom enable
+/etc/init.d/firewall-custom start
+/etc/init.d/irqbalance enable
+/etc/init.d/irqbalance start
+/etc/init.d/dnsmasq enable
+/etc/init.d/dnsmasq start
 /etc/rc.local enable
+/etc/rc.local start
 
 rm -rf /root/*
 reboot
