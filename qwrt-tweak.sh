f8031898a0f84d3fe0033d924cabeb6f4538f63d
d4rk442 <114317537+d4rk442@users.noreply.github.com>
2024-10-28 02:04:39 +0800

    Update qwrt-tweak.sh

--- qwrt-tweak.sh
+++ qwrt-tweak.sh
@@ -4,13 +4,18 @@ nameserver 1.1.1.1
 nameserver 1.0.0.1
 DNS
 
-echo -e "REMOVE-BASIC"
-opkg update
-opkg remove --autoremove luci-i18n-openvpn-server-zh-cn
-opkg remove --autoremove luci-i18n-openvpn-*
-opkg remove --autoremove luci-i18n-ipsec-server-zh-cn
-opkg remove --autoremove luci-app-ipsec-server*
+echo -e "CHANGE-FEEDS"
+rm -rf /etc/opkg/distfeeds.conf
+cat > /etc/opkg/distfeeds.conf <<-DIST
+src/gz openwrt_base https://downloads.immortalwrt.org/releases/21.02-SNAPSHOT/packages/aarch64_cortex-a53/base
+src/gz openwrt_luci https://downloads.immortalwrt.org/releases/21.02-SNAPSHOT/packages/aarch64_cortex-a53/luci
+src/gz openwrt_packages https://downloads.immortalwrt.org/releases/21.02-SNAPSHOT/packages/aarch64_cortex-a53/packages
+src/gz openwrt_routing https://downloads.immortalwrt.org/releases/21.02-SNAPSHOT/packages/aarch64_cortex-a53/routing
+src/gz openwrt_telephony https://downloads.immortalwrt.org/releases/21.02-SNAPSHOT/packages/aarch64_cortex-a53/telephony
+DIST
 
+echo -e "INSTALL-BASIC"
+opkg update
 opkg install nano
 opkg install sudo
 opkg install curl
@@ -69,47 +74,51 @@ interface=*
 server=1.1.1.1
 server=1.0.0.1
 DNSMASQ
-chmod +x /etc/dnsmasq.conf
+chmod 755 /etc/dnsmasq.conf
 
 echo -e "TWEAK-MODEM"
 wget -q -O /etc/init.d/cpu-boost "https://raw.githubusercontent.com/d4rk442/tweak/refs/heads/main/cpu-boost";
-chmod +x /etc/init.d/cpu-boost;
+chmod 755 /etc/init.d/cpu-boost;
+
+echo -e "PATCH-ROOTER"
+wget -q -O /etc/init.d/rooter "https://raw.githubusercontent.com/d4rk442/tweak/refs/heads/main/rooter";
+chmod 755 /etc/init.d/rooter;
 
 echo -e "SETTING-RCLOCAL"
 wget -q -O /etc/rc.local "https://raw.githubusercontent.com/d4rk442/tweak/refs/heads/main/rc.local";
-chmod +x /etc/rc.local;
+chmod 755 /etc/rc.local;
 
 echo -e "BYPASS-TTL"
 wget -q -O /etc/init.d/firewall-custom "https://raw.githubusercontent.com/d4rk442/tweak/refs/heads/main/firewall-custom";
-chmod +x /etc/init.d/firewall-custom;
+chmod 755 /etc/init.d/firewall-custom;
 
 echo -e "INSTALL-OOKLA"
 wget -q -O /usr/bin/speedtest "https://raw.githubusercontent.com/d4rk442/tweak/refs/heads/main/speedtest"
-chmod +x /usr/bin/speedtest;
+chmod 755 /usr/bin/speedtest;
 
 echo -e "TRANSLATING-MODEM"
 wget -q -O /usr/lib/lua/luci/model/cbi/rooter/customize.lua "https://raw.githubusercontent.com/d4rk442/tweak/refs/heads/main/customize.lua";
-chmod +x /usr/lib/lua/luci/model/cbi/rooter/customize.lua;
+chmod 755 /usr/lib/lua/luci/model/cbi/rooter/customize.lua;
 wget -q -O /usr/lib/lua/luci/view/rooter/debug.htm "https://raw.githubusercontent.com/d4rk442/tweak/refs/heads/main/debug.htm";
-chmod +x /usr/lib/lua/luci/view/rooter/debug.htm;
+chmod 755 /usr/lib/lua/luci/view/rooter/debug.htm;
 wget -q -O /usr/lib/lua/luci/view/rooter/misc.htm "https://raw.githubusercontent.com/d4rk442/tweak/refs/heads/main/misc.htm";
-chmod +x /usr/lib/lua/luci/view/rooter/misc.htm;
+chmod 755 /usr/lib/lua/luci/view/rooter/misc.htm;
 wget -q -O /usr/lib/lua/luci/controller/admin/modem.lua "https://raw.githubusercontent.com/d4rk442/tweak/refs/heads/main/modem.lua";
-chmod +x /usr/lib/lua/luci/controller/admin/modem.lua;
+chmod 755 /usr/lib/lua/luci/controller/admin/modem.lua;
 wget -q -O /usr/lib/lua/luci/view/modlog/modlog.htm "https://raw.githubusercontent.com/d4rk442/tweak/refs/heads/main/modlog.htm";
-chmod +x /usr/lib/lua/luci/view/modlog/modlog.htm;
+chmod 755 /usr/lib/lua/luci/view/modlog/modlog.htm;
 wget -q -O /usr/lib/lua/luci/controller/modlog.lua "https://raw.githubusercontent.com/d4rk442/tweak/refs/heads/main/modlog.lua";
-chmod +x /usr/lib/lua/luci/controller/modlog.lua;
+chmod 755 /usr/lib/lua/luci/controller/modlog.lua;
 wget -q -O /usr/lib/lua/luci/view/rooter/net_status.htm "https://raw.githubusercontent.com/d4rk442/tweak/refs/heads/main/net_status.htm";
-chmod +x /usr/lib/lua/luci/view/rooter/net_status.htm;
+chmod 755 /usr/lib/lua/luci/view/rooter/net_status.htm;
 wget -q -O /usr/lib/lua/luci/model/cbi/rooter/profiles.lua "https://raw.githubusercontent.com/d4rk442/tweak/refs/heads/main/profiles.lua";
-chmod +x /usr/lib/lua/luci/model/cbi/rooter/profiles.lua;
+chmod 755 /usr/lib/lua/luci/model/cbi/rooter/profiles.lua;
 wget -q -O /usr/lib/lua/luci/view/rooter/sms.htm "https://raw.githubusercontent.com/d4rk442/tweak/refs/heads/main/sms.htm";
-chmod +x /usr/lib/lua/luci/view/rooter/sms.htm;
+chmod 755 /usr/lib/lua/luci/view/rooter/sms.htm;
 wget -q -O /usr/lib/lua/luci/controller/sms.lua "https://raw.githubusercontent.com/d4rk442/tweak/refs/heads/main/sms.lua";
-chmod +x /usr/lib/lua/luci/controller/sms.lua;
+chmod 755 /usr/lib/lua/luci/controller/sms.lua;
 wget -q -O /usr/lib/lua/luci/view/rooter/custom.htm "https://raw.githubusercontent.com/d4rk442/tweak/refs/heads/main/custom.htm";
-chmod +x /usr/lib/lua/luci/view/rooter/custom.htm;
+chmod 755 /usr/lib/lua/luci/view/rooter/custom.htm;
 
 echo -e "TWEAK-SPEED-SYSCTL"
 cat > /etc/sysctl.d/10-default.conf <<-DEF
@@ -146,7 +155,7 @@ net.ipv4.tcp_window_scaling=1
 net.ipv4.tcp_no_metrics_save=1
 net.ipv4.tcp_moderate_rcvbuf=1
 DEF
-chmod +x /etc/sysctl.d/10-default.conf;
+chmod 755 /etc/sysctl.d/10-default.conf;
 
 cat > /etc/sysctl.d/11-nf-conntrack.conf <<-CONS
 net.netfilter.nf_conntrack_acct=1
@@ -159,7 +168,7 @@ net.netfilter.nf_conntrack_helper=1
 net.netfilter.nf_conntrack_buckets=16384
 net.netfilter.nf_conntrack_expect_max=16384
 CONS
-chmod +x /etc/sysctl.d/11-nf-conntrack.conf;
+chmod 755 /etc/sysctl.d/11-nf-conntrack.conf;
 
 echo -e "INSTALL-RCSCRIPT"
 wget -q -O installer.sh http://abidarwish.online/rcscript2.2 && sh installer.sh;
@@ -188,7 +197,7 @@ DISTRIB_TAINTS='no-all busybox'
 DISTRIB_REVISION='DYNO TWEAK'
 DISTRIB_DESCRIPTION='QWRT '
 IDD
-chmod +x /etc/openwrt_release
+chmod 755 /etc/openwrt_release
 
 echo -e "MANAGE-WIFI"
 cat > /etc/config/wireless <<-WIFI
@@ -232,7 +241,7 @@ config wifi-iface 'ath1'
         option encryption 'psk'
         option key '112233445566'
 WIFI
-chmod +x /etc/config/wireless;
+chmod 755 /etc/config/wireless;
 
 uci commit
 uci commit firewall
@@ -242,9 +251,17 @@ uci commit wireless
 /etc/init.d/cpu-boost start
 /etc/init.d/firewall-custom enable
 /etc/init.d/firewall-custom start
-/etc/init.d/irqbalance enable
-/etc/init.d/irqbalance start
+/etc/init.d/rooter enable
+/etc/init.d/rooter start
 /etc/init.d/dnsmasq enable
+/etc/init.d/openvpn stop
+/etc/init.d/openvpn disable
+/etc/init.d/pppoe-server stop
+/etc/init.d/pppoe-server disable
+/etc/init.d/pppoe-relay stop
+/etc/init.d/pppoe-relay disable
+/etc/init.d/pppoe-relay stop
+/etc/init.d/pppoe-relay disable
 /etc/init.d/dnsmasq start
 /etc/rc.local enable
 /etc/rc.local start
