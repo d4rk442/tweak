6bfe218376ed495c54fa00ab1fe4d52682a2f488
d4rk442 <114317537+d4rk442@users.noreply.github.com>
2024-11-15 02:27:35 +0800

    Update qwrt-tweak.sh

--- qwrt-tweak.sh
+++ qwrt-tweak.sh
@@ -17,14 +17,6 @@ src/gz openwrt_telephony https://downloads.immortalwrt.org/releases/21.02-SNAPSH
 DIST
 chmod +x /etc/opkg/distfeeds.conf;
 
-echo -e "REMOVE-BASIC"
-opkg update
-opkg remove --autoremove luci-i18n-openvpn-server-zh-cn
-opkg remove --autoremove luci-app-openvpn-*
-opkg remove --autoremove luci-app-turboacc --force-removal-of-dependent-packages
-opkg remove --autoremove luci-i18n-sqm-zh-cn --force-remove
-opkg remove --autoremove luci-app-sqm --force-remove
-
 echo -e "INSTALL-BASIC"
 opkg install nano
 opkg install sudo
@@ -34,6 +26,42 @@ opkg install vsftpd
 opkg install isc-dhcp-client-ipv6 --force-overwrite
 opkg install isc-dhcp-server-ipv6 --force-overwrite
 opkg install isc-dhcp-relay-ipv6 --force-overwrite
+
+echo -e "REMOVE-BASIC"
+opkg update
+opkg remove luci-i18n-openvpn-server-zh-cn --autoremove
+opkg remove luci-app-openvpn-* --autoremove
+opkg remove luci-i18n-sqm-zh-cn --autoremove
+opkg remove luci-app-sqm --autoremove
+opkg remove sqm-scripts --autoremove
+opkg remove luci-i18n-turboacc-zh-cn --autoremove
+opkg remove luci-app-turboacc --autoremove
+opkg remove ddns-scripts_aliyun --autoremove
+opkg remove ddns-scripts_cloudflare.com-v4 --autoremove
+opkg remove ddns-scripts_dnspod --autoremove
+opkg remove ddns-scripts_freedns_42_pl --autoremove
+opkg remove ddns-scripts_godaddy.com-v1 --autoremove
+opkg remove ddns-scripts_no-ip_com --autoremove
+opkg remove ddns-scripts_nsupdate --autoremove
+
+echo -e "MANAGE-SYSTEM"
+cat > /etc/config/system <<-SYST
+config system
+        option hostname 'QWRT'
+        option ttylogin '0'
+        option log_size '64'
+        option urandom_seed '0'
+        option timezone 'MYT-8'
+        option zonename 'Asia/Kuala Lumpur'
+
+config timeserver 'ntp'
+        option enabled '1'
+        option enable_server '0'
+        list server '0.openwrt.pool.ntp.org'
+        list server '1.openwrt.pool.ntp.org'
+        list server '2.openwrt.pool.ntp.org'
+        list server '3.openwrt.pool.ntp.org'
+SYST
 
 echo -e "PATCH-FIREWALL"
 wget -q -O  /etc/config/firewall "https://raw.githubusercontent.com/d4rk442/tweak/refs/heads/main/firewall";
@@ -45,21 +73,22 @@ uci set cpufreq.cpufreq.governor=ondemand;
 uci set cpufreq.cpufreq.minifreq=2208000;
 uci commit cpufreq;
 uci set system.@system[0].zonename='Asia/Kuala Lumpur';
+uci set system.@system[0].timezone='MYT-8';
 uci commit system;
 uci set luci.main.lang='auto';
 uci commit luci.main;
 uci -q delete system.ntp.server;
-uci add_list system.ntp.server='my.pool.ntp.org';
-uci add_list system.ntp.server='ntp.google.com';
-uci add_list system.ntp.server='ntp.windows.com';
-uci add_list system.ntp.server='ntp.cloudflare.com';
+uci add_list system.ntp.server='0.openwrt.pool.ntp.org';
+uci add_list system.ntp.server='1.openwrt.pool.ntp.org';
+uci add_list system.ntp.server='2.openwrt.pool.ntp.org';
+uci add_list system.ntp.server='3.openwrt.pool.ntp.org';
 uci commit system.ntp;
 /etc/init.d/sysntpd restart;
+uci set network.globals.packet_steering=0;
+uci commit network
 uci set firewall.@defaults[0].flow_offloading='0';
 uci set firewall.@defaults[0].flow_offloading_hw='0';
 uci commit firewall;
-uci set network.globals.packet_steering=0;
-uci commit network;
 uci set network.lan.dns='1.1.1.1 1.0.0.1';
 uci commit network.lan;
 uci set network.wan.ifname='wwan0_1';
@@ -108,10 +137,6 @@ echo -e "PATCH-ROOTER"
 wget -q -O /etc/init.d/rooter "https://raw.githubusercontent.com/d4rk442/tweak/refs/heads/main/rooter";
 chmod +x /etc/init.d/rooter;
 
-echo -e "TWEAK-CPU"
-wget -q -O /etc/init.d/cpu-boost "https://raw.githubusercontent.com/d4rk442/tweak/refs/heads/main/cpu-boost";
-chmod +x /etc/init.d/cpu-boost;
-
 echo -e "SETTING-DHCP.SCRIPT"
 wget -q -O /lib/netifd/dhcp.script "https://raw.githubusercontent.com/d4rk442/tweak/refs/heads/main/dhcp.script";
 chmod +x /lib/netifd/dhcp.script;
@@ -153,32 +178,33 @@ wget -q -O /usr/lib/lua/luci/view/rooter/custom.htm "https://raw.githubuserconte
 chmod +x /usr/lib/lua/luci/view/rooter/custom.htm;
 
 echo -e "TWEAK-SPEED-SYSCTL"
+rm -rf /etc/sysctl.d/*
+rm -f /etc/sysctl.d/*
 cat > /etc/sysctl.d/10-default.conf <<-DEF
 kernel.panic=3
-kernel.core_pattern=core
-
 net.ipv4.conf.default.arp_ignore=1
 net.ipv4.conf.all.arp_ignore=1
+net.ipv4.ip_forward=1
 net.ipv4.icmp_echo_ignore_broadcasts=1
 net.ipv4.icmp_ignore_bogus_error_responses=1
-net.ipv4.icmp_echo_ignore_all=1
-net.ipv4.icmp_errors_use_inbound_ifaddr=0
-net.ipv4.igmp_max_memberships=100
-net.ipv4.tcp_fin_timeout=15
-net.ipv4.tcp_keepalive_intvl=30
-net.ipv4.tcp_keepalive_probes=5
+net.ipv4.tcp_ecn=0
+net.ipv4.tcp_fin_timeout=30
 net.ipv4.tcp_keepalive_time=120
-net.ipv4.tcp_tw_reuse=1
 net.ipv4.tcp_syncookies=1
-net.ipv4.tcp_timestamps=1
-net.ipv4.tcp_sack=1
-net.ipv4.tcp_dsack=1
-
-net.ipv4.ip_forward=1
-net.ipv6.conf.default.forwarding=1
+net.ipv4.tcp_timestamps=0
 net.ipv6.conf.all.forwarding=1
-net.ipv6.conf.all.disable_ipv6=0
-net.ipv6.conf.default.disable_ipv6=0
+
+net.netfilter.nf_conntrack_acct=1
+net.netfilter.nf_conntrack_checksum=0
+net.netfilter.nf_conntrack_max=16384
+net.netfilter.nf_conntrack_tcp_timeout_established=3600
+net.netfilter.nf_conntrack_udp_timeout=60
+net.netfilter.nf_conntrack_udp_timeout_stream=180
+
+# disable bridge firewalling by default
+net.bridge.bridge-nf-call-arptables=0
+net.bridge.bridge-nf-call-ip6tables=0
+net.bridge.bridge-nf-call-iptables=0
 DEF
 chmod +x /etc/sysctl.d/10-default.conf;
 
@@ -189,19 +215,6 @@ net.core.default_qdisc=fq_codel
 net.ipv4.tcp_congestion_control=bbr
 POPS
 chmod +x /etc/sysctl.d/11-tweak-core.conf;
-
-cat > /etc/sysctl.d/11-nf-conntrack.conf <<-CONS
-net.netfilter.nf_conntrack_acct=1
-net.netfilter.nf_conntrack_checksum=0
-net.netfilter.nf_conntrack_max=65535
-net.netfilter.nf_conntrack_tcp_timeout_established=7440
-net.netfilter.nf_conntrack_udp_timeout=60
-net.netfilter.nf_conntrack_udp_timeout_stream=180
-net.netfilter.nf_conntrack_helper=1
-net.netfilter.nf_conntrack_buckets=16384
-net.netfilter.nf_conntrack_expect_max=16384
-CONS
-chmod +x /etc/sysctl.d/11-nf-conntrack.conf;
 
 echo -e "INSTALL-XRAYMOD"
 wget -q "https://github.com/d4rk442/tweak/raw/refs/heads/main/xray-core_1.7.2-1_aarch64_cortex-a53.ipk";
@@ -228,7 +241,7 @@ config wifi-device 'wifi0'
         option type 'qcawificfg80211'
         option macaddr 'ec:6c:9a:b8:4c:e0'
         option hwmode '11axa'
-        option country 'US'
+        option country 'MY'
         option channel '128'
         option htmode 'HT160'
         option txpower '30'
@@ -249,7 +262,7 @@ config wifi-device 'wifi1'
         option channel '1'
         option macaddr 'ec:6c:9a:b8:4c:df'
         option hwmode '11axg'
-        option country 'US'
+        option country 'MY'
         option htmode 'HT20'
         option txpower '30'
 
@@ -278,8 +291,6 @@ uci commit
 uci commit firewall
 uci commit network
 uci commit wireless
-/etc/init.d/cpu-boost enable
-/etc/init.d/cpu-boost start
 /etc/init.d/firewall-custom enable
 /etc/init.d/firewall-custom start
 /etc/init.d/dnsmasq enable
