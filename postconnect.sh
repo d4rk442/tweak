f7ab4ae11327a3d04d560bbc67cee6848fe6edde
d4rk442 <114317537+d4rk442@users.noreply.github.com>
2024-12-23 00:28:38 +0800

    Update postconnect.sh

--- postconnect.sh
+++ postconnect.sh
@@ -21,18 +21,21 @@ nameserver 2606:4700:4700::1111
 nameserver 2606:4700:4700::1001
 DNS001
 
-cat > /etc/resolv.conf <<-DNS002
+cat > /tmp/resolv.conf.d/resolv.conf.auto <<-DNSSF
 nameserver 1.1.1.1
 nameserver 1.0.0.1
 nameserver 2606:4700:4700::1111
 nameserver 2606:4700:4700::1001
+DNSSF
+
+cat > /etc/resolv.conf <<-DNS002
+nameserver 127.0.0.1
+nameserver ::1
 DNS002
 
 cat > /tmp/resolv.conf <<-DNS003
-nameserver 1.1.1.1
-nameserver 1.0.0.1
-nameserver 2606:4700:4700::1111
-nameserver 2606:4700:4700::1001
+nameserver 127.0.0.1
+nameserver ::1
 DNS003
 
 if [ -e /usr/lib/scan/emailchk.sh ]; then
